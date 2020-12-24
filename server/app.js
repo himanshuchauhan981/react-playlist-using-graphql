712f544d536d35f2423339538ffedecec40db3b8
himanshuchauhan981 <himanshuchauhan981@gmail.com>
2020-12-24 16:28:29 +0530

    added author schema

--- server/app.js
+++ server/app.js
@@ -5,7 +5,7 @@ const schema = require('./schema/schema');
 
 const app = express();
 
-app.use('/graphql', graphqlHTTP({ schema }));
+app.use('/graphql', graphqlHTTP({ schema, graphiql: true }));
 
 app.listen(4000, () => {
 	console.log('Running on port 4000');
